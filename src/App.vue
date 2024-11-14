407779cebfd366bfd683e9b694716455e1825b94
ms0ur <maksimsuslov7142@gmail.com>
2024-11-14 16:33:49 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- Wrap IonApp with KonstaProvider with theme="parent" -->
+  <k-provider theme="parent">
+    <ion-app>
+      <ion-router-outlet />
+    </ion-app>
+  </k-provider>
+</template>
+
+<script lang="ts">
+import { kProvider } from 'konsta/vue';
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    IonApp,
+    IonRouterOutlet,
+    kProvider,
+  },
+});
+</script>
+
+<style>
+
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps(['header'])
+</script>
+
+<template>
+  <header class="w-full prosto text-start text-2xl ">
+    <h1>
+      {{ props.header }}
+    </h1>
+  </header>
+
+</template>
+
+<style scoped>
+header{
+  background: #1B98E0;
+  padding: 10px 20px 10px 20px;
+  font-size: 40px;
+}
+</style>
